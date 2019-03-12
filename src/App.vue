b117b5c9b6acb829a3008829c6b53c711c926c14
BosEriko <bos.eriko@gmail.com>
2019-03-12 19:43:41 +0800

    Moved the Home to pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <router-view/>
   </div>
 </template>
@@ -10,5 +10,5 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 </style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      msg: "Under Maintenance"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
