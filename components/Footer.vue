5bb72d646b92424b83a2464f925bb8c7174bbbb6
BosEriko <bos.eriko@gmail.com>
2019-10-10 07:47:54 +0800

    add header and footer components

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    Footer!
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="container">
-    Under Maintenance
+  <div>
+    <Header />
+    <div class="container">
+      I want you to be happier.
+    </div>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
