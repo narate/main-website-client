48041bda9ce9cfd091e5034b676e31b8f583fabf
BosEriko <bos.eriko@gmail.com>
2019-10-10 08:40:27 +0800

    add kuru and kuruDark color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        kuru: `#CD3C2A`,
+        kuruDark: `#982618`
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
